bf77c6d992be4598533713c12e12f3dcd5a55d7b
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2025-02-03 15:10:49 +0300

    doing routing for files credentials

--- book-recommendation-client/src/App.jsx
+++ book-recommendation-client/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/login";
+import Search from "./pages/Search";
+import BookDetails from "./pages/BookDetails";
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/search" element={<Search />} />
+                <Route path="/books" element={<BookDetails />} />
+            </Routes>
+        </Router>
+    );
 }
 
-export default App
+export default App;
+
