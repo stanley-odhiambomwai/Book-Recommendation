001ae994a95ff8fd7c43dd29f6f84b4e63262fe4
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2025-02-03 14:20:08 +0300

    api for directories

--- Server/Src/Server.js
+++ Server/Src/Server.js
@@ -19,5 +19,5 @@ app.use(helmet());
  app.use("/api/users", userRoutes);
 app.use("/api/books", bookRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ book-recommendation-client/src/services/api.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const API = axios.create({ baseURL: "http://localhost:3000/api" });
+
+export const searchBooks = (query) => API.get(`/books/search?query=${query}`);
+export const registerUser = (userData) => API.post("/users/register", userData);
+export const loginUser = (userData) => API.post("/users/login", userData);
+export const getAllBooks = () => API.get("/books");
+export const addBook = (bookData, token) => API.post("/books/add", bookData, { headers: { Authorization: token } });
